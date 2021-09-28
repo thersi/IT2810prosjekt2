518cf9688356c636e4e27cb87fde5e320d5a3f2c
Victoria Hopso <victoria.hopso@gmail.com>
2021-09-28 10:13:12 +0200

    Implement darkmode(#13)

--- Gr37/src/App.tsx
+++ Gr37/src/App.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
 import './App.css';
 import Search from './components/Search'
 import BarChart from './components/BarChart'
 import LineChart from './components/LineChart'
+import { ThemeCtx} from './ThemeProvider'
+
 
 
 function App() {
+  const { theme, toggleTheme } = useContext(ThemeCtx);
+  let bar = false; /*implement means of setting the condition
+  based on the input to the search component*/
+
+
+  const renderCorrectChart = () => {
+    if (bar) {
+      return <BarChart
+      xAxis={['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
+      yAxis={[12, 19, 3, 5, 2, 3]}
+    />
+    } else {
+      return <LineChart
+      dates={['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
+      merges={[12, 19, 3, 5, 2, 3]}
+    />;
+    }
+  }
+
   return (
-    <div className="App">
-      <Search/>
-      <LineChart
-        dates={['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
-        merges={[12, 19, 3, 5, 2, 3]}
-      />
-      <BarChart
-        xAxis={['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
-        yAxis={[12, 19, 3, 5, 2, 3]}
-      />
-    </div>
+      <div className="App">
+        <button id= 'toggleTheme' onClick={toggleTheme}>
+        Switch to {theme === "light" ? "dark" : "light"} mode
+      </button>
+        <Search></Search>
+        <div className="displayChart">
+        {renderCorrectChart()}
+        </div>
+      </div>
   );
 }
 
---
+++ Gr37/src/ThemeProvider.tsx
@@ -0,0 +1,28 @@
+
+import React, { useState } from "react";
+
+type Theme = "light" | "dark";
+type ThemeContext = { theme: Theme; toggleTheme: () => void };
+
+export const ThemeCtx = React.createContext<ThemeContext>(
+  {} as ThemeContext
+);
+
+export const ThemeProvider: React.FC = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>("light");
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const txtColor = theme === "light" ? "#333" : "#FFF";
+  const backgroundColor = theme === "light" ? "#FFF" : "#333";
+
+  document.body.style.color = txtColor;
+  document.body.style.backgroundColor = backgroundColor;
+
+  return (
+    <ThemeCtx.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeCtx.Provider>
+  );
+};
