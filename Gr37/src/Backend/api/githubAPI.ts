ebcd0c047eaff42672f23fb5f56cee4c5498c6a3
thersi <therese.sigmundstad@gmail.com>
2021-09-28 15:41:30 +0200

    Fix async promise issue

--- Gr37/src/Backend/api/githubAPI.ts
+++ Gr37/src/Backend/api/githubAPI.ts
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
 
 //Metoden tar inn url som github repoet
-export default function getEvents(){
-var arrayEvents = new Array()
-const request= 'https://gitlab.stud.idi.ntnu.no/api/v4/projects/11908/events';
-const token = 'Sct5JeQqxKoTAw1Smgyc';
-const events= fetch(request, { method: 'GET',
- headers: { 'private-token': 'Sct5JeQqxKoTAw1Smgyc', 'content-type': 'application/json'}})
- .then(response => response.json())
- .then( data => {for(var i = 0; i < data.length; i++) {
+export default function gevents() {
+  var arrayEvents = new Array();
+  const request =
+    "https://gitlab.stud.idi.ntnu.no/api/v4/projects/11908/events";
+  const token = "Sct5JeQqxKoTAw1Smgyc";
+  const events = fetch(request, {
+    method: "GET",
+    headers: {
+      "private-token": "Sct5JeQqxKoTAw1Smgyc",
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      for (var i = 0; i < data.length; i++) {
+        arrayEvents.push(data[i]);
+      }
+    })
 
-     
-    arrayEvents.push(data[i]);
- }})
-     
- .catch( error => console.error(error));
- 
-return arrayEvents}
+    .catch((error) => console.error(error));
+
+  return arrayEvents;
+}
 
 /* 
 export function getAll(){
