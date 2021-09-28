ef8dfba14e506a08892840d880fdf3a8230e59f1
thersi <therese.sigmundstad@gmail.com>
2021-09-28 16:12:00 +0200

    Add getEvent function in app

--- Gr37/src/App.tsx
+++ Gr37/src/App.tsx
@@ -1,17 +1,29 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./App.css";
 import Search from "./components/Search";
 import BarChart from "./components/BarChart";
 import LineChart from "./components/LineChart";
 import { ThemeCtx } from "./ThemeProvider";
-import getEvents from "./Backend/api/githubAPI";
+import getEvents from "./Backend/api/GithubFetch";
 
 function App() {
-  console.log(getEvents());
+  const [getData, setData] = useState();
+
+  const fetchData = async () => {
+    setData(await getEvents());
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   const { theme, toggleTheme } = useContext(ThemeCtx);
   let bar = false; /*implement means of setting the condition
   based on the input to the search component*/
-
+  if (getData === undefined) {
+    return <div>Loading....</div>;
+  }
+  console.log(getData[0]);
+  const ids = getData.map((data: any) => <div>{data.id}</div>);
   const renderCorrectChart = () => {
     if (bar) {
       return (
