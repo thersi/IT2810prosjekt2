672cf72865851af3e4e08777d9ca3fa7e44912df
thersi <therese.sigmundstad@gmail.com>
2021-09-28 11:42:11 +0200

    Add getEvents log to app

--- Gr37/src/App.tsx
+++ Gr37/src/App.tsx
@@ -1,42 +1,43 @@
 import React, { useContext, useState } from "react";
-import './App.css';
-import Search from './components/Search'
-import BarChart from './components/BarChart'
-import LineChart from './components/LineChart'
-import { ThemeCtx} from './ThemeProvider'
-
-
+import "./App.css";
+import Search from "./components/Search";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import { ThemeCtx } from "./ThemeProvider";
+import getEvents from "./Backend/api/githubAPI";
 
 function App() {
+  console.log(getEvents());
   const { theme, toggleTheme } = useContext(ThemeCtx);
   let bar = false; /*implement means of setting the condition
   based on the input to the search component*/
 
-
   const renderCorrectChart = () => {
     if (bar) {
-      return <BarChart
-      xAxis={['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
-      yAxis={[12, 19, 3, 5, 2, 3]}
-    />
+      return (
+        <BarChart
+          xAxis={["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]}
+          yAxis={[12, 19, 3, 5, 2, 3]}
+        />
+      );
     } else {
-      return <LineChart
-      dates={['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
-      merges={[12, 19, 3, 5, 2, 3]}
-    />;
+      return (
+        <LineChart
+          dates={["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]}
+          merges={[12, 19, 3, 5, 2, 3]}
+        />
+      );
     }
-  }
+  };
 
   return (
-      <div className="App">
-        <button id= 'toggleTheme' onClick={toggleTheme}>
+    <div className="App">
+      <button id="toggleTheme" onClick={toggleTheme}>
         Switch to {theme === "light" ? "dark" : "light"} mode
       </button>
-        <Search></Search>
-        <div className="displayChart">
-        {renderCorrectChart()}
-        </div>
-      </div>
+      <Search></Search>
+      <div className="displayChart">{renderCorrectChart()}</div>
+    </div>
   );
 }
 
