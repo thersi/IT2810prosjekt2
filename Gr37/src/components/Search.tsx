cd37212d62739abafca0ed5fd7a75d9bea2505d2
Erlend Vaboen <erlevab@stud.ntnu.no>
2021-09-28 09:57:47 +0200

    Update Search

--- Gr37/src/components/Search.tsx
+++ Gr37/src/components/Search.tsx
@@ -1,70 +1,65 @@
-import React, { useState } from "react"
-import { Card, Select, MenuItem, SelectChangeEvent, TextField, Button} from '@mui/material';
+import React, { useState, Fragment } from "react"
+import { Select, MenuItem, TextField, Button, Grid, FormControl, InputLabel, Box } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { LocalizationProvider, DatePicker } from '@mui/lab';
+import { LocalizationProvider, DateRangePicker, DateRange } from '@mui/lab';
 
 export default function Search() {
     const [value, setValue] = useState('');
-    const [fromDate, setFromDate] = useState<Date | null>(new Date())
-    const [toDate, setToDate] = useState<Date | null>(new Date());
+    const [dates, setDates] = useState<DateRange<Date>>([null, null]);
 
-
-    const handleValueChange = (event: SelectChangeEvent) => {
-        setValue(event.target.value as string);
-    }
-
-    const handleFromDateChange = (newValue: Date | null) => {
-        if (newValue != null) {
-            if (toDate != null && newValue.getTime() <= toDate.getTime()) { 
-                setFromDate(newValue);
-            }
-        console.log(fromDate)
-    }
-    }
-
-    const handleToDateChange = (newValue: Date | null) => {
-        if (newValue != null) {
-            if (fromDate != null && newValue.getTime() >= fromDate.getTime()) { 
-                setToDate(newValue);
-            }
-        console.log(toDate)
-    }
-    }
 
     const onSearch = () => {
-        console.log(fromDate, toDate, value)
+        console.log(dates, value)
     }
 
     return (
-        <Card>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={value}
-                label="Age"
-                onChange={handleValueChange}
-            >
-                <MenuItem value={'Issues'}>Issues</MenuItem>
-                <MenuItem value={'Commits'}>Commits</MenuItem>
-            </Select>
-            
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DatePicker
-                    label="From"
-                    value={fromDate}
-                    onChange={handleFromDateChange}
-                    renderInput={(params) => <TextField {...params} />}
-                />
-                <DatePicker
-                    label="To"
-                    value={toDate}
-                    onChange={handleToDateChange}
-                    renderInput={(params) => <TextField {...params} />}
-                />
-            </LocalizationProvider>
-            <Button variant="contained" onClick={onSearch}>Search</Button>
-        </Card>
-
+        <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={1}
+            justifyContent='center'
+            alignItems='center'
+            padding={2}>
+            <Grid item xs={12} sm={3}>
+                <FormControl fullWidth>
+                    <InputLabel id="select-label">Action</InputLabel>
+                    <Select
+                        labelId="select-label"
+                        id="select"
+                        value={value}
+                        label="Action"
+                        onChange={(event) => {
+                            setValue(event.target.value as string)
+                        }} >
+                        <MenuItem value={'Issues'}>Issues</MenuItem>
+                        <MenuItem value={'Commits'}>Commits</MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <FormControl fullWidth >
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DateRangePicker
+                            disableFuture
+                            calendars={1}
+                            value={dates}
+                            onChange={(newValue) => {
+                                setDates(newValue);
+                            }}
+                            renderInput={(startProps, endProps) => (
+                                <Fragment >
+                                    <TextField fullWidth {...startProps} />
+                                    <Box sx={{ mx: 1 }}> to </Box>
+                                    <TextField fullWidth {...endProps} />
+                                </Fragment>
+                            )}
+                        />
+                    </LocalizationProvider>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={1} marginRight={1}>
+                <Button variant="contained" onClick={onSearch}>Search</Button>
+            </Grid>
+        </Grid>
     )
-
 }
