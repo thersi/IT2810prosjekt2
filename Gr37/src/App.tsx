7cf69f86fa251e6db095449fd9db0f3d37cb6095
@haakhor <haakhor@stud.ntnu.no>
2021-09-30 08:46:52 +0200

    Add localstorage function

--- Gr37/src/App.tsx
+++ Gr37/src/App.tsx
@@ -5,13 +5,10 @@ import BarChart from './components/BarChart'
 import LineChart from './components/LineChart'
 import { ThemeCtx} from './ThemeProvider'
 
-
-
 function App() {
   const { theme, toggleTheme } = useContext(ThemeCtx);
   let bar = false; /*implement means of setting the condition
   based on the input to the search component*/
-
 
   const renderCorrectChart = () => {
     if (bar) {
@@ -27,12 +24,22 @@ function App() {
     }
   }
 
+  const c = sessionStorage.getItem('count');
+  const dflt_c = JSON.parse(c !== null ? c : "0");
+  const [count] = useState<number>(dflt_c);
+
+  window.onload = e => {
+    var n = count+1;
+    sessionStorage.setItem('count', JSON.stringify(n));
+    console.log("number of reloads: ", n);
+  }
+
   return (
       <div className="App">
         <button id= 'toggleTheme' onClick={toggleTheme}>
         Switch to {theme === "light" ? "dark" : "light"} mode
       </button>
-        <Search></Search>
+        <Search/>
         <div className="displayChart">
         {renderCorrectChart()}
         </div>
