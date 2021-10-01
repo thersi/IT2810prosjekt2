4d4316cabfd7786d1195aabac2d6cf5d093ae855
Victoria Hopso <victoria.hopso@gmail.com>
2021-10-01 11:41:31 +0200

    additional snapshot test (#14)

--- Gr37/src/tests/snapshotTests.test.tsx
+++ Gr37/src/tests/snapshotTests.test.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Search from '../components/Search'
 import BarChart from '../components/BarChart'
+import LineChart from '../components/LineChart'
 import App from '../App'
 import { cleanup, render } from '@testing-library/react'
 import renderer from 'react-test-renderer'
 
 afterEach(cleanup);
 let div: HTMLDivElement;
-
 
 
 it('Search renders without crashing', () => {
@@ -27,3 +27,16 @@ it('matches snapshot', () => {
     expect(tree).toMatchSnapshot();
 })
 
+ it('renders linechart correctly', () => {
+    const x: any = Array('Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange')
+    const y: any = Array(12, 19, 3, 5, 2, 3)
+    const tree = renderer.create(<LineChart
+        dates={x} merges={y}/>).toJSON
+    expect(tree).toMatchSnapshot();
+ })
+
+ it('matches snapshot', () => {
+    const tree = renderer.create(<BarChart xAxis={['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
+    yAxis={[12, 19, 3, 5, 2, 3]}/>).toJSON()
+    expect(tree).toMatchSnapshot();
+})
