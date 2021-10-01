432e019ee65283a2f9e75752a11397b56df6e735
thersi <therese.sigmundstad@gmail.com>
2021-10-01 16:56:07 +0200

    Update url for fetching api data

--- Gr37/src/Backend/api/GithubFetch.tsx
+++ Gr37/src/Backend/api/GithubFetch.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 //Metoden tar inn url som github repoet
 export default async function getEvents() {
   const request = new Request(
-    "https://gitlab.stud.idi.ntnu.no/api/v4/projects/11908/events",
+    "https://gitlab.stud.idi.ntnu.no/api/v4/projects/11908/events?per_page=500&action=pushed",
     {
       method: "GET",
       headers: {
@@ -17,27 +17,7 @@ export default async function getEvents() {
   const data = await fetch(request)
     .then((r) => r.json())
     .catch((error) => console.log(error));
-  console.log(data)
-
-  return data;
-}
-export async function getAll() {
-  const request = new Request(
-    "https://gitlab.stud.idi.ntnu.no/api/v4/projects/11908",
-    {
-      method: "GET",
-      headers: {
-        "private-token": "Sct5JeQqxKoTAw1Smgyc",
-        "Content-Type": "application/x-www-form-urlencoded",
-        Accept: "application/json",
-      },
-    }
-  );
-
-  const data = await fetch(request)
-    .then((r) => r.json())
-    .catch((error) => console.log(error));
-  console.log(data)
+  console.log(data);
 
   return data;
 }
