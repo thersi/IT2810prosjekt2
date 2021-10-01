3e3b9a28dcafd8c9bcfa92d1dcf9fb624fb70d90
thersi <therese.sigmundstad@gmail.com>
2021-10-01 17:34:31 +0200

    Add changes

--- Gr37/src/Backend/api/GithubFetch.tsx
+++ Gr37/src/Backend/api/GithubFetch.tsx
@@ -21,3 +21,30 @@ export default async function getEvents() {
 
   return data;
 }
+
+export async function getMerge() {
+  const request = new Request(
+    "https://gitlab.stud.idi.ntnu.no/api/v4/projects/11908/events?per_page=500&action=pushed",
+    {
+      method: "GET",
+      headers: {
+        /*
+        In a production environment, or some application that is to be deployed, including a
+        key in such a manner is a very bad practice. Instead, it should be stored externally,
+        either by passing the request through some back-end service or by using a framework
+        such as NextJS to store the values in a .env file.
+        */
+        "private-token": "Sct5JeQqxKoTAw1Smgyc",
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+      },
+    }
+  );
+
+  const data = await fetch(request)
+    .then((r) => r.json())
+    .catch((error) => console.log(error));
+  console.log(data);
+
+  return data;
+}
